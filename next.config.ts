2f50baaf712999b7cf9f82b6affab5f9d4e4a7f9
sempakmumbul <avidmuhammad128@gmail.com>
2026-07-10 03:05:15 +0700

    perbaikan config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Baris ini wajib ada agar folder 'out' terbentuk saat build
-  basePath: '/berita', 
   images: {
-    unoptimized: true, 
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
